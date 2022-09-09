eaf69d3b899675a89287f0de9e288cf5bbf509bd
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-09-09 17:49:37 +0300

    j

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import { Provider } from 'react-redux';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from 'components/App';
+import './index.css';
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <persistGate loading={null} persistor={persistor}> */}
+    
+    <App/>
+    
+    {/* </persistGate> */}
+  </React.StrictMode>
+);
